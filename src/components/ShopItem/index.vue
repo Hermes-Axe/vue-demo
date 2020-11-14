5d72817dfaed6e220f2c64f56349537b2b80a585
dengchao <dengchao@gongmall.com>
2020-11-14 23:10:07 +0800

    fix: shop-item组件重构

--- src/components/ShopItem/index.vue
+++ src/components/ShopItem/index.vue
@@ -1,32 +1,49 @@
 <template>
   <div id="shop-item">
     <div class="shop-item-left">
-      <!-- https://i0.hdslb.com/bfs/bangumi/1d703634cd3ee35b625bf882f27289db301cae63.jpg@87w_88h_1c_100q.webp -->
-      <img :src="shopImgUrl" :alt="shopName">
+      <img :src="shop.shopImgUrl" :alt="shop.shopName+'-img'">
     </div>
     <div class="shop-item-right">
       <!-- 店名 -->
-      <!-- <div class="shop-name">华莱士·全鸡汉堡(仓前杭师大店)</div> -->
-      <div class="shop-name">{{ shopName }}</div>
+      <div class="shop-name">
+        <span class="name">{{ shop.shopName }}</span>
+        <i class="iconfont icon-ziyuan"></i>
+      </div>
       <!-- 评分 -->
       <div class="score-salenumber-timedistance clearfix">
-        <span class="score">4.6分</span>
-        <span class="salenumber">月售4845</span>
-        <span class="timedistance">30分钟 695m</span>
+        <div>
+          <span class="score">
+            <span>{{ shop.score }}</span><span>分</span>
+          </span>
+          <span class="salenumber">月售{{ shop.monthData }}</span>
+        </div>
+        <div>
+          <span class="timedistance">{{ shop.time }}分钟 {{ shop.distance }}m</span>
+        </div>
       </div>
       <!-- 起送价 -->
-      <div class="price clearfix">
-        <span class="start-price">起送价￥20</span>
-        <span class="night-send-price">夜间配送￥1</span>
-        <span class="driver">蓝骑士专送</span>
+      <div class="price">
+        <div>
+          <span class="start-price">起送价￥20</span>
+          <span class="night-send-price" v-if="shop.isNight">夜间配送￥1</span>
+        </div>
+        <div>
+          <span class="driver">蓝骑士专送</span>
+        </div>
       </div>
       <!-- 店铺标签 -->
-      <div class="discription">"香辣鸡腿堡棒棒哒"</div>
-      <br>
+      <div class="discription">
+        <span>"{{ shop.tag }}"</span>
+      </div>
       <!-- tag -->
-      <div class="active-tags">49减34 | 69减44</div>
-      <i class="iconfont icon-ziyuan"></i>
-      <i class="iconfont icon-jiantou9"></i>
+      <div class="active-tags">
+        <div class="tags">
+          <span>49减34</span><span>69减44</span>
+        </div>
+        <div>
+          <i class="iconfont icon-jiantou9"></i>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,142 +51,131 @@
 <script>
 export default {
   props: {
-    shopName: {type: String, required: true},
-    shopImgUrl: {type: String, required: true}
+    shop: {
+      type: Object,
+      required: true,
+
+      isNight: Boolean,
+      shopName: { type: String, required: true },
+      shopImgUrl: { type: String, required: true },
+      score: { type: Number, required: true },
+      monthData: { type: Number },
+      time: Number,
+      distance: Number,
+      tag: String
+    }
+    // shop.shopName: { type: String, required: true },
+    // shopImgUrl: { type: String, required: true },
+    // score: { type: Number, required: true }
   }
 }
 </script>
 
 <style lang="scss">
 #shop-item {
-  width: 100%;
-  height: 160px;
-  font-size: 14px;
-  line-height: 20px;
-  color: #444;
-  background: #fff;
   display: flex;
-  justify-content: space-between;
+  align-items: top;
+  justify-content: flex-start;
+  background-color: #fff;
   border-radius: 6px;
+  padding: 10px;
   box-sizing: border-box;
-  // border: 1px solid #000;
-  padding: 16px 20px;
-  position: relative;
-  div {
-    margin-bottom: 4px;
-  }
+  flex: 1;
   .shop-item-left {
-    display: flex;
-    flex-direction: column;
-    // justify-content: space-around;
-    margin-bottom: 0;
-    width: 30%;
+    width: 40%;
     img {
-      width: 100%;
-      // height: 100%;
+      display: block;
+      width: 100px;
+      height: 100px;
       border-radius: 6px;
-      transform: translateY(50%);
+      background: red;
     }
   }
   .shop-item-right {
-    width: 70%;
-    padding-left: 10px;
+    width: 60%;
+    font-size: 16px;
+    display: flex;
+    flex-direction: column;
+    // align-content: space-between;
+    justify-content: space-between;
     .shop-name {
-      max-width: 80%;
-      font-size: 16px;
-      line-height: 22px;
-      font-weight: bold;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .name {
+        display: block;
+        max-width: 12em;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
     }
     .score-salenumber-timedistance {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 12px;
       color: #444;
-      line-height: 20px;
-      vertical-align: bottom;
-      max-width: 100%;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
       .score {
-        float: left;
-        font-size: 16px;
-        margin-right: 6px;
-        color: brown;
-      }
-      .salenumber {
-        float: left;
-      }
-      .timedistance {
-        float: right;
-        max-width: 20%;
-        overflow: hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
+        color: tomato;
+        span:first-child {
+          font-size: 16px;
+        }
+        span:last-child {
+          font-size: 14px;
+        }
       }
     }
     .price {
-      font-size: 0;
-      max-width: 100%;
-      white-space: nowrap;
-      overflow: hidden;
-      .start-price {
-        font-size: 14px;
-        margin-right: 6px;
-        float: left;
-      }
-      .night-send-price {
-        font-size: 14px;
-        float: left;
-        max-width: 10%;
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-      }
+      font-size: 12px;
+      line-height: 14px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      color: #444;
       .driver {
-        font-size: 14px;
-        float: right;
-        color: blue;
-        background-color: skyblue;
+        font-size: 12px;
+        padding: 2px;
         box-sizing: border-box;
-        padding: 0 4px;
-        border-radius: 3px;
+        border-radius: 2px;
+        background-color: rgb(108, 179, 184);
+        color: rgb(80, 123, 173);
       }
     }
     .discription {
-      display: inline-block;
-      border-radius: 3px;
-      padding: 2px 3px;
-      color: brown;
-      background-color: rgb(185, 139, 100);
+      font-size: 12px;
+      span {
+        line-height: 14px;
+        color: red;
+        background: tomato;
+        border-radius: 2px;
+        padding: 2px;
+      }
     }
     .active-tags {
-      display: inline-block;
-      color: brown;
-      border-radius: 3px;
-      border: 1px solid brown;
-      margin-bottom: 0;
-    }
-    .iconfont {
-      position: absolute;
-      font-size: 20px;
-      &.icon-ziyuan {
-        top: 12px;
-        right: 20px;
+      margin-top: 4px;
+      font-size: 12px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .tags {
+        background-color: #fff;
+        color: tomato;
+        padding: 4px;
+        border: 1px solid tomato;
+        border-radius: 4px;
+        span {
+          border-right: 1px solid tomato;
+          padding: 0 4px;
+          &:first-child {
+            padding-left: 0;
+          }
+          &:last-child {
+            border: none;
+            padding-right: 0;
+          }
+        }
       }
-      &.icon-jiantou9 {
-        bottom: 20px;
-        right: 20px;
-      }
-    }
-  }
-  .clearfix {
-    zoom: 1;
-    &::after {
-      content: '';
-      display: block;
-      height: 0;
-      clear: both;
     }
   }
 }
--- src/pages/Shop/index.vue
+++ src/pages/Shop/index.vue
@@ -1,12 +1,32 @@
 <template>
-  <ShopItem></ShopItem>
+  <div>
+    <div v-for="(item, index) in shopList" :key="index">
+      <shop-item :shop="item"></shop-item>
+    </div>
+  </div>
 </template>
 
 <script>
 import ShopItem from '@components/ShopItem'
 export default {
   components: {
-    ShopItem
+    'shop-item': ShopItem
+  },
+  data () {
+    return {
+      shopList: [
+        {
+          shopName: '华莱士·全鸡汉堡(仓前杭师大店)',
+          shopImgUrl: 'https://i0.hdslb.com/bfs/bangumi/1d703634cd3ee35b625bf882f27289db301cae63.jpg@87w_88h_1c_100q.webp',
+          score: 4.9,
+          monthData: 100,
+          time: 356,
+          distance: 666,
+          isNight: false,
+          tag: 'lalala'
+        }
+      ]
+    }
   }
 }
 </script>
